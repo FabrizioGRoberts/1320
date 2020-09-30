9d68788d732ffd5f0c1cd0949585601fece0df18
FabrizioGRoberts <Robertsfg5220@gmail.com>
2020-09-30 14:34:59 -0600

    Delete compress.m

--- compress.m
+++
@@ -1,4 +0,0 @@
-function [compAudioVec] = compress(audioVec, compression_ratio)
-	% compress compresses the input audio to the inputted compression ratio
-	compAudioVec = audioVec(1:compression_ratio:end);
-end
